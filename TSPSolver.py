9a0bf0f538e5c52b8c18e9791b7c7e0f16edc682
Christian Bain <christianbain@me.com>
2020-12-09 15:15:39 -0700

    commented d+c B+B

--- TSPSolver.py
+++ TSPSolver.py
@@ -350,102 +350,43 @@ class TSPSolver:
 		best solution found.  You may use the other three field however you like.
 		algorithm</returns> 
 	'''
+
+    
     def fancy(self, time_allowance=60.0):
-        dists = self._scenario.getCities()
 
-        """
-        Implementation of Held-Karp, an algorithm that solves the Traveling
-        Salesman Problem using dynamic programming with memoization.
-        Parameters:
-            dists: distance matrix
-        Returns:
-            A tuple, (cost, path).
-        """
-        n = len(dists)
+        '''
+        divide and conquer branch and bound
 
-        # Maps each subset of the nodes to the cost to reach that subset, as well
-        # as what node it passed before reaching this subset.
-        # Node subsets are represented as set bits.
-        C = {}
+        divide the problem into n sets of x cities
 
-        # Set transition cost from initial state
-        for k in range(1, n):
-            C[(1 << k, k)] = (dists[0][k], 0)
+        solve each one with branch and bound
 
-        # Iterate subsets of increasing length and store intermediate results
-        # in classic dynamic programming manner
-        for subset_size in range(2, n):
-            for subset in itertools.combinations(range(1, n), subset_size):
-                # Set bits for all nodes in this subset
-                bits = 0
-                for bit in subset:
-                    bits |= 1 << bit
+        build back up to final solution
 
-                # Find the lowest cost to get to this subset
-                for k in subset:
-                    prev = bits & ~(1 << k)
+        must find more optimal solution than greedy
 
-                    res = []
-                    for m in subset:
-                        if m == 0 or m == k:
-                            continue
-                        res.append((C[(prev, m)][0] + dists[m][k], m))
-                    C[(bits, k)] = min(res)
+        :param time_allowance:
+        :return:
+        '''
 
-        # We're interested in all bits but the least significant (the start state)
-        bits = (2 ** n - 1) - 1
+        # dists = self._scenario.getCities()
 
-        # Calculate optimal cost
-        res = []
-        for k in range(1, n):
-            res.append((C[(bits, k)][0] + dists[k][0], k))
-        opt, parent = min(res)
+        pass
 
-        # Backtrack to find full path
-        path = []
-        for i in range(n - 1):
-            path.append(parent)
-            new_bits = bits & ~(1 << parent)
-            _, parent = C[(bits, parent)]
-            bits = new_bits
 
-        # Add implicit start state
-        path.append(0)
 
-        return opt, list(reversed(path))
 
-    def generate_distances(n):
-        dists = [[0] * n for i in range(n)]
-        for i in range(n):
-            for j in range(i + 1, n):
-                dists[i][j] = dists[j][i] = random.randint(1, 99)
 
-        return dists
 
-    def read_distances(filename):
-        dists = []
-        with open(filename, 'rb') as f:
-            for line in f:
-                # Skip comments
-                if line[0] == '#':
-                    continue
 
-                dists.append(map(int, map(str.strip, line.split(','))))
 
-        return dists
 
-    # if __name__ == '__main__':
-    #     arg = sys.argv[1]
-    #
-    #     if arg.endswith('.csv'):
-    #         dists = read_distances(arg)
-    #     else:
-    #         dists = generate_distances(int(arg))
-    #
-    #     # Pretty-print the distance matrix
-    #     for row in dists:
-    #         print(''.join([str(n).rjust(3, ' ') for n in row]))
-    #
-    #     print('')
 
-        # print(fancy(dists))
+
+
+
+
+
+
+
+
